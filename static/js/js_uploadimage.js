1c38fd5647e55ffd783224d44f1f5805089012f4
marwanmostafa9 <marwan.m16112000@gmail.com>
2023-05-11 06:07:19 +0300

    added the rest of js files

---
+++ static/js/js_uploadimage.js
@@ -0,0 +1,50 @@
+function previewImage(event) {
+    var previewContainer = document.getElementById('preview-container');
+    previewContainer.innerHTML = ''; // clear previous preview
+
+    var file = event.target.files[0];
+    var imageType = /^image\//;
+
+    if (!imageType.test(file.type)) {
+        previewContainer.innerHTML = "Please select an image file.";
+        return;
+    }
+
+    var img = document.createElement("img");
+    img.classList.add("preview-image");
+
+    var reader = new FileReader();
+    reader.onload = function() {
+        img.src = reader.result;
+        document.getElementById("upload-btn").classList.remove("hidden");
+        document.getElementById("back-btn").classList.remove("hidden");
+    }
+    reader.readAsDataURL(file);
+
+    previewContainer.appendChild(img);
+    }
+
+function additionalAction() {
+// Code to perform additional action goes here
+}
+
+function showButtons() {
+document.getElementById("upload-btn").classList.remove("hidden");
+document.getElementById("back-btn").classList.remove("hidden");
+}
+
+function uploadFile() {
+    event.preventDefault(); // Prevent form submission
+    
+    var fileInput = document.getElementById("myFile");
+    var file = fileInput.files[0];
+    
+    var img = document.createElement("img");
+    var objectURL = URL.createObjectURL(file);
+    
+    img.onload = function() {
+        URL.revokeObjectURL(this.src);
+    }
+    
+    img.src = objectURL;
+}
