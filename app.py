d6de6724b197f43fbac7c34f5b65700c8bc60d12
Abdel-Rahman-Sameh <20190292@stud.fci.edu.eg>
2023-05-11 11:32:16 +0300

    added the PDF functions to the file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import paddleocr
+import PyPDF2
 import os
 
 app = Flask(__name__)
@@ -45,6 +46,66 @@ def process_image():
         
         return render_template("html_output.html", result=text)
     
+    except Exception as e:
+        return f"Error processing image: {e}"
+
+
+@app.route('/upload_pdf', methods=['GET','POST'])
+def upload_pdf():
+    return render_template("html_uploadpdf.html", pagetitle="Upload PDF")
+
+
+def extract_text_from_pdf(pdf_file):
+    # extract text from uploaded PDF    
+    try:
+        pdfFileObj = PyPDF2.PdfReader(pdf_file)
+        numPages = len(pdfFileObj.pages)  # pdfFileObj.getNumPages()
+        text = ''
+        for pageNum in range(numPages):
+            pageObj = pdfFileObj.pages[pageNum]
+            text += pageObj.extract_text()
+
+        print("///////////////////////////")
+        print("The pdf processing result is...")
+        print(text)
+
+        # return result
+        return text
+
+        # redirect to summarization route
+        # return redirect(url_for('generate_summary'))
+    except Exception as e:
+        return {'result': f'Error processing PDF file: {str(e)}'}
+    
+    
+    
+    # try:
+    #     pdf_file = request.files['pdf-file']
+    #     extracted_text = process_pdf(pdf_file)
+
+
+
+    #     # summarize extracted text
+    #     # summary = generate_summary(extracted_text)
+
+    #     # do something with the summary, like display it on a page
+    #     rendered = render_template("html_output.html", result=extracted_text)
+    #     return rendered
+    # except Exception as e:
+    #     print("---- EXCEPTION FOUND ----")
+    #     print(e)
+
+@app.route("/process_pdf", methods=['GET','POST'])
+def process_pdf():
+    try:
+        # check if a file was uploaded
+        if 'pdf-file' not in request.files:
+            return {'result': 'No file uploaded'}
+
+        # get the uploaded file
+        pdf_file = request.files['pdf-file']
+        text = extract_text_from_pdf(pdf_file)
+        return render_template("html_output.html", result=text)
     except Exception as e:
         return f"Error processing image: {e}"
 
