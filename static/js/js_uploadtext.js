5a0ccc0fe5b2755282d7d9e3a834853b8221e2c5
Abdel-Rahman-Sameh <abdelrahmansamehh2000@gmail.com>
2023-06-30 17:13:46 +0300

    added the style and res options for text

--- static/js/js_uploadtext.js
+++ static/js/js_uploadtext.js
@@ -1,5 +1,11 @@
 function submitForm() {
     var input = document.getElementById("myTextarea").value;
+    var styleOption = document.getElementById("style-options-dropdown").value;
+    var resolutionOption = document.getElementById("resolution-options-dropdown").value;
+
     document.getElementById("myInput").value = input;
+    document.getElementById("style-option-input").value = styleOption;
+    document.getElementById("resolution-option-input").value = resolutionOption;
     document.getElementById("myForm").submit();
 }
+  
